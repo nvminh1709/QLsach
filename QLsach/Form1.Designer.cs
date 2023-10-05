5ba3698803118421e68160a1ed2bc57c7f430f0c
vklvk <vklvk@NGUYENMINH>
2023-10-05 17:41:30 +0700

    Add project files.

---
+++ QLsach/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace QLsach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtmasach = new System.Windows.Forms.TextBox();
+            this.txtsoluong = new System.Windows.Forms.TextBox();
+            this.txtgia = new System.Windows.Forms.TextBox();
+            this.txttensach = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnthemmoi = new System.Windows.Forms.Button();
+            this.btncapnhat = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã sách";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên sách";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giá";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(422, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số lượng";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtmasach
+            // 
+            this.txtmasach.Location = new System.Drawing.Point(240, 62);
+            this.txtmasach.Name = "txtmasach";
+            this.txtmasach.Size = new System.Drawing.Size(100, 22);
+            this.txtmasach.TabIndex = 4;
+            // 
+            // txtsoluong
+            // 
+            this.txtsoluong.Location = new System.Drawing.Point(503, 135);
+            this.txtsoluong.Name = "txtsoluong";
+            this.txtsoluong.Size = new System.Drawing.Size(100, 22);
+            this.txtsoluong.TabIndex = 5;
+            // 
+            // txtgia
+            // 
+            this.txtgia.Location = new System.Drawing.Point(503, 65);
+            this.txtgia.Name = "txtgia";
+            this.txtgia.Size = new System.Drawing.Size(100, 22);
+            this.txtgia.TabIndex = 6;
+            // 
+            // txttensach
+            // 
+            this.txttensach.Location = new System.Drawing.Point(240, 135);
+            this.txttensach.Name = "txttensach";
+            this.txttensach.Size = new System.Drawing.Size(100, 22);
+            this.txttensach.TabIndex = 7;
+            this.txttensach.TextChanged += new System.EventHandler(this.txttensach_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(148, 203);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(455, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // btnthemmoi
+            // 
+            this.btnthemmoi.Location = new System.Drawing.Point(148, 399);
+            this.btnthemmoi.Name = "btnthemmoi";
+            this.btnthemmoi.Size = new System.Drawing.Size(95, 23);
+            this.btnthemmoi.TabIndex = 9;
+            this.btnthemmoi.Text = "Thêm mới ";
+            this.btnthemmoi.UseVisualStyleBackColor = true;
+            this.btnthemmoi.Click += new System.EventHandler(this.btnthemmoi_Click);
+            // 
+            // btncapnhat
+            // 
+            this.btncapnhat.Location = new System.Drawing.Point(274, 399);
+            this.btncapnhat.Name = "btncapnhat";
+            this.btncapnhat.Size = new System.Drawing.Size(100, 23);
+            this.btncapnhat.TabIndex = 10;
+            this.btncapnhat.Text = "Cập nhật";
+            this.btncapnhat.UseVisualStyleBackColor = true;
+            this.btncapnhat.Click += new System.EventHandler(this.btncapnhat_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(407, 399);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(95, 23);
+            this.btnxoa.TabIndex = 11;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(520, 399);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(83, 23);
+            this.btnthoat.TabIndex = 12;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 450);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btncapnhat);
+            this.Controls.Add(this.btnthemmoi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txttensach);
+            this.Controls.Add(this.txtgia);
+            this.Controls.Add(this.txtsoluong);
+            this.Controls.Add(this.txtmasach);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtmasach;
+        private System.Windows.Forms.TextBox txtsoluong;
+        private System.Windows.Forms.TextBox txtgia;
+        private System.Windows.Forms.TextBox txttensach;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnthemmoi;
+        private System.Windows.Forms.Button btncapnhat;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnthoat;
+    }
+}
+
---
+++ QLsach/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLsach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnthemmoi_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=NGUYENMINH;Initial Catalog=qlsach;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("insert into bang values('"+txtmasach.Text+ "','"+txttensach.Text+ "','"+Convert.ToDouble(txtgia.Text)+ "','"+Convert.ToInt16(txtsoluong.Text)+"')", conn);
+            conn.Open();
+            int ret=cmd.ExecuteNonQuery();
+            if (ret == 1) {
+                MessageBox.Show ("them du lieu thanh cong");
+            }
+            conn.Close();
+        }
+
+        private void txttensach_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btncapnhat_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=NGUYENMINH;Initial Catalog=qlsach;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("update bang set tensach='" + txttensach.Text + "',gia='" + Convert.ToDouble(txtgia.Text) + "',soluong='" + Convert.ToInt16(txtsoluong.Text) + "'where masach='" + txtmasach.Text + "'", conn);
+            conn.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if (ret == 1)
+            {
+                MessageBox.Show("cap nhat du lieu thanh cong");
+            }
+            conn.Close();
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=NGUYENMINH;Initial Catalog=qlsach;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("delete from bang where masach='" + txtmasach.Text + "'", conn);
+            conn.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if (ret == 1)
+            {
+                MessageBox.Show("xoa du lieu thanh cong");
+            }
+            conn.Close();
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    
+}
